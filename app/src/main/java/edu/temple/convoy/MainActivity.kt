335624e1f347bdf17cf5c4e2045d4feb2b833f9a
FrancisRScallatino <FrancisRScallatino@gmail.com>
2024-02-14 12:03:48 -0500

    add 2 TextFields and buttons
    need to figure out how to change this first page dynamically. not working yet

--- app/src/main/java/edu/temple/convoy/MainActivity.kt
+++ app/src/main/java/edu/temple/convoy/MainActivity.kt
@@ -3,13 +3,16 @@ package edu.temple.convoy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
 import edu.temple.convoy.ui.theme.ConvoyTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +20,27 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ConvoyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                var isMember by remember{ mutableStateOf(true) }
+
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+                    if (!isMember) OutlinedTextField_Composable("Full name")
+                    OutlinedTextField_Composable("Username")
+                    OutlinedTextField_Composable("Password")
+
+                    if (isMember){
+                        Button_Composable("Log in!")
+
+                        Text(
+                            text = "Don't have an account?",
+                            modifier = Modifier.clickable { isMember = true }
+                        )
+                    }else{
+                        Button_Composable("Sign up!")
+                    }
                 }
             }
         }
@@ -30,14 +48,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun OutlinedTextField_Composable(nameOfLabel : String){
+    var text by remember{ mutableStateOf("") }
+    OutlinedTextField(
+        value = text,
+        onValueChange = {text = it},
+        label = { Text(nameOfLabel)},
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            unfocusedBorderColor = MaterialTheme.colors.secondary
+        ),
+        keyboardOptions = KeyboardOptions.Default.copy(
+            imeAction = ImeAction.Done
+        ),
+        visualTransformation = if(text.isEmpty()) VisualTransformation.None else VisualTransformation.None
+    )
 }
 
+@Composable
+fun Button_Composable(nameOfButton : String){
+    Button(onClick = {
+
+    }) {
+        Text(text = nameOfButton)
+    }
+}
+
+@Composable
+fun Text_Composable(nameOfButton : String){
+    Button(onClick = {
+
+    }) {
+        Text(text = nameOfButton)
+    }
+}
+
+/*
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ConvoyTheme {
         Greeting("Android")
     }
-}
+}*/
